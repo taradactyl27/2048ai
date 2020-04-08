98671f69c1e577c267652240e6f840f9ce104d86
taradactyl27 <ataradachuk@DESKTOP-HTP1CCG.localdomain>
2020-04-07 23:02:51 -0400

    done

--- PlayerAI_3.py
+++ PlayerAI_3.py
@@ -1,3 +1,7 @@
+# Alex Taradachuk
+# Prof. Raja
+# CSCI - 350
+# Assignment 2 - 2048 AI
 import random
 import collections
 import math
@@ -8,32 +12,32 @@ class PlayerAI(BaseAI):
 
 	def getMove(self, grid):
 		score = float('-inf')
-		bestMove = 1
-		for i in range(3):
-			tempGrid = grid.clone()
-			if tempGrid.move(i+1):
-				emptyCells = len(tempGrid.getAvailableCells())
-				if emptyCells > 7:
-					moveScore = self.expectimax(tempGrid,2,False)
+		bestMove = 1 
+		for i in range(3): #automatically prune up position given nature of heuristic - the snake pattern runs downwards leading to no necessary case where an up move is needed
+			tempGrid = grid.clone() # clone the grid
+			if tempGrid.move(i+1): #move in chosen direction
+				emptyCells = len(tempGrid.getAvailableCells()) #get number of free cells to be used for iterative deepening
+				if emptyCells > 12:
+					moveScore = self.expectimax(tempGrid,2,False) #run at an effective depth of 3
 				else:
-					if emptyCells > 3:
-						moveScore = self.expectimax(tempGrid,3,False)
+					if emptyCells > 5:
+						moveScore = self.expectimax(tempGrid,3,False) #run at an effective depth of 4
 					else:
-						moveScore = self.expectimax(tempGrid,4,False)
+						moveScore = self.expectimax(tempGrid,4,False) #run at an effective depth of 5
 				if moveScore > score:
-					score = moveScore
-					bestMove = i+1
+					score = moveScore #check if score has been beaten
+					bestMove = i+1 #update best move 
 		return bestMove
 
-	def expectimax(self, grid, depth, isPlayer):
+	def expectimax(self, grid, depth, isPlayer): #expectimax algorithm used to determine best move
 		if isPlayer and not grid.canMove():
-			return -999999
-		if depth == 0:
-			return self.getScore(grid)
-		if not isPlayer:
+			return -999999 #if grid cannot move then return worst score
+		if depth == 0: #reached leaf node
+			return self.getScore(grid) #evaluation function
+		if not isPlayer: #expected layer
 			scoreT = 0
 			length = 0
-			for cell in grid.getAvailableCells():
+			for cell in grid.getAvailableCells(): #set value of each cell to 2 or 4 and run expectimax on each variation
 				temp = grid.clone()
 				length += 1
 				temp.setCellValue(cell, 2)
@@ -41,25 +45,24 @@ class PlayerAI(BaseAI):
 				temp = grid.clone()
 				temp.setCellValue(cell, 4)
 				scoreT += 0.1 * self.expectimax(temp,depth-1,True)
-				# grid.setCellValue(cell,0)
 			return scoreT/length if length > 0 else -999999
-		if isPlayer:
-			scoreT = -999999
-			for i in range(3):
+		if isPlayer: #max node
+			scoreT = -999999 #start with worst score
+			for i in range(3): #check for each possible move
 				temp = grid.clone()
 				temp.move(i+1)
-				scoreT = max(scoreT,self.expectimax(temp,depth-1,False))
+				scoreT = max(scoreT,self.expectimax(temp,depth-1,False)) #max of every possible successor
 			return scoreT
 	
 	def getScore(self, grid):
 		score = 1
-		weightMatrix = [[0,1,2,3],
+		weightMatrix = [[0,1,2,3],        #use a snake weight matrix that keeps largest tiles in bottom corner
 						[7,6,5,4],
 						[8,9,12,14],
 						[24,18,16,15]]
 		penalty = 1
 		previousPos = -1
-		for i in range (4):
+		for i in range (4):    #iterate through grid to get weighted dot product and also evaluate the penalty based on differences between neighbors
 			for j in range(4):
 				pos = grid.map[i][j]
 				if previousPos != -1:
@@ -71,12 +74,8 @@ class PlayerAI(BaseAI):
 						penalty += abs(grid.map[i-1][j] - pos)
 					if(i + 1 <= 3):
 						penalty += abs(grid.map[i+1][j] - pos)
-			previousPos = -1
-		
-		# print("SCORE ", math.log2(score*2))
-		# print("PENALTY:", math.log2(penalty/8))			
-		heur = score - penalty*2 + len(grid.getAvailableCells())**2
-		#- math.log2(penalty/16)
+			previousPos = -1		
+		heur = score - penalty + len(grid.getAvailableCells()) #combine weighted dot product, smoothness, and available cells as final heuristic
 		return heur
 		 
 				
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# 2048ai
+# 2048ai - Alex Taradachuk
+
+I used expecitmax with weighted heuristics for my approach to the problem. For heuristics I used a weighted dot product, with a snake matrix, smoothness, and free cells. After playing around with multiple weights, I found the best performance to be given by default values with certain weights applied during evaluation.
